a7c4a8e60021e22dcffbdc206725d302d6888f0e
SynysterZV <greendayfanj@gmail.com>
2020-12-01 05:58:35 -0500

    Added Kick/Ban

---
+++ commands/ban.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'ban',
+    description: 'ban',
+    usage: '{mention} {reason}',
+    async execute(args, message) {
+        const getUserFromMention = require('./getUserFromMention');
+
+        if (args.length < 2) {
+            return message.reply('Please mention the user you want to ban and specify a reason');
+        }
+
+        const user = getUserFromMention(args[0]);
+        if (!user) {
+            return message.reply('Please use a proper mention');
+        }
+
+        const reason = args.slice(1).join(' ');
+        try {
+            await message.guild.members.ban(user, { reason });
+        }
+        catch (error) {
+            return message.channel.send(`Failed to ban **${user.tag}**: ${error}`);
+        }
+        return message.channel.send(`Successfully banned **${user.tag}** from the server!`);
+    },
+};
